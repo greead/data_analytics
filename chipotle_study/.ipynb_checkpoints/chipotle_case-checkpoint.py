53df24d53c2b4196851271267a4723231f1fb6bb
greead04 <129024691+greead04@users.noreply.github.com>
2023-08-31 02:25:04 -0400

    added chipotle study and unemployment study

---
+++ chipotle_study/.ipynb_checkpoints/chipotle_case-checkpoint.py
@@ -0,0 +1,10 @@
+# Imports
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+chipotle_df = pd.read_csv('chipotle_data.txt', delimiter="\t")
+print(chipotle_df.dtypes)
+
+
